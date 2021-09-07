e3522340a4770e375f1374888dd9965f712cb706
Lais Telles <telleslais.93@gmail.com>
2021-09-07 20:27:59 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/cursolais/cursomc/domain/Produto.java
+++ src/main/java/com/cursolais/cursomc/domain/Produto.java
@@ -13,6 +13,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 	
@@ -26,6 +28,7 @@ public class Produto implements Serializable {
 	
 	
 	//abaixo vou fazer a tabela que faz o meio de campo de N para N(Várias categorias para vários produtos
+	@JsonBackReference
 	@ManyToMany	
 	@JoinTable(	name = "PRODUTO_CATEGORIA",
 				joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/cursolais/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursolais/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cursolais.cursomc.domain.Categoria;
 import com.cursolais.cursomc.repositories.CategoriaRepository;
+import com.cursolais.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: "+ id + ", Tipo:" + Categoria.class.getName());
+		}
 		return obj.get();
+		//return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:"+ id + " Tipo:"+ Categoria.class.getName()));
 	}
 
 }
