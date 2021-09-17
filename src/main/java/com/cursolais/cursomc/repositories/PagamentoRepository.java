85fa42d7a1a24fa0ce23cd6ecb462b77c2790b86
Lais Telles <telleslais.93@gmail.com>
2021-09-17 17:57:10 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/cursolais/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,13 @@
+package com.cursolais.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursolais.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+	
+	
+
+}
